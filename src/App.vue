525949881af149d992ced85afab89d8aa35db74a
DescomCesar <cgarciam@descom.es>
2024-05-06 15:40:49 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <WeatheR></WeatheR>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import  WeatheR from "@/components/WeatheR.vue";
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/WeatheR.vue
@@ -0,0 +1,22 @@
+<template>
+    <main class="wather"></main>
+    <button @click="callApi">buscar</button>
+    <input v-if="response">{{ response }}<input>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const country = ref("")
+
+const response = ref("")
+
+const callApi =  async () => {
+    const responsehttp = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m/" + country.value)
+    responsehttp.value = await responsehttp.json();
+}
+
+</script>
+
+
+<style scoped></style>
